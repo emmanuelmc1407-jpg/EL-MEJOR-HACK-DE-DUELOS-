a0b16457338c4185ebf6ce7b4b45b3312f8904b8
emmanuelmc1407-jpg <emmanuelmc1407@gmail.com>
2026-02-28 21:34:26 -0500

    Create Duelos.lua

---
+++ Duelos.lua
@@ -0,0 +1,8 @@
+-- Script de Prueba para el Repositorio de Emmanuel
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "¡Conexión Exitosa!";
+    Text = "Tu script de GitHub se ha ejecutado correctamente.";
+    Duration = 10;
+})
+
+print("El script 'Duelos.lua' está corriendo desde GitHub.")
